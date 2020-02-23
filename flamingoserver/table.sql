d0086f188a9ca59415e9abeef9a8c98f9b1da2c1
ml422524 <326479531@qq.com>
2020-02-23 18:14:17 +0800

    modify config, table.sql

--- flamingoserver/table.sql
+++ flamingoserver/table.sql
@@ -1,4 +1,4 @@
-//用户信息表
+
 CREATE TABLE IF NOT EXISTS  t_user  (
          f_id  bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增ID',
 		 f_user_id  bigint(20) NOT NULL COMMENT '用户ID',
@@ -22,34 +22,22 @@ CREATE TABLE IF NOT EXISTS  t_user  (
          PRIMARY KEY ( f_user_id ),
 		 INDEX f_user_id (f_user_id),
          KEY  f_id  ( f_id )
-        ) ENGINE=InnoDB DEFAULT CHARSET=utf8
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-//alter table t_user add f_teaminfo blob default null comment "好友分组信息";
-//alter table t_user drop column f_teaminfo;
-
-//用户关系表
-//为了避免冗余，一定要保证f_user_id1小于f_user_id2
 CREATE TABLE IF NOT EXISTS  t_user_relationship  (
          f_id  bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增ID',
          f_user_id1  bigint(20) NOT NULL COMMENT '第一个用户id',
          f_user_id2  bigint(20) NOT NULL COMMENT '第二个用户id',		 
-		 f_user1_teamname VARCHAR(32) NOT NULL DEFAULT "我的好友" COMMENT "用户2在用户1的好友分组名称",
+		 f_user1_teamname VARCHAR(32) NOT NULL DEFAULT 'my good friend' COMMENT "用户2在用户1的好友分组名称",
 		 f_user1_markname VARCHAR(32) COMMENT "用户2在用户1的备注名称",		 
-		 f_user2_teamname VARCHAR(32) NOT NULL DEFAULT "我的好友" COMMENT "用户1在用户2的好友分组名称",
+		 f_user2_teamname VARCHAR(32) NOT NULL DEFAULT "my good friend" COMMENT "用户1在用户2的好友分组名称",
 		 f_user2_markname VARCHAR(32) COMMENT "用户1在用户2的备注名称",
          f_update_time  timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
          f_remark  varchar(64) DEFAULT NULL COMMENT '备注',
          PRIMARY KEY ( f_id ),
          KEY  f_id  ( f_id )
-        ) ENGINE=InnoDB DEFAULT CHARSET=utf8
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-//ALTER TABLE t_user_relationship CHANGE f_create_time f_update_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间';		
-//alter table t_user_relationship modify f_user1_teamname varchar(32) not null default "我的好友" comment "用户2在用户1的好友分组名称";
-//alter table t_user_relationship modify f_user2_teamname varchar(32) not null default "我的好友" comment "用户1在用户2的好友分组名称";
-//alter table t_user_relationship add f_user1_markname VARCHAR(32) COMMENT "用户2在用户1的备注名称";
-//alter table t_user_relationship add f_user2_markname VARCHAR(32) COMMENT "用户1在用户2的备注名称";
-
-//消息记录表
 CREATE TABLE IF NOT EXISTS  t_chatmsg  (
          f_id  bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增ID',
          f_senderid  bigint(20) NOT NULL COMMENT '发送者id',
@@ -59,4 +47,4 @@ CREATE TABLE IF NOT EXISTS  t_chatmsg  (
          f_remark  varchar(64) DEFAULT NULL COMMENT '备注',
          PRIMARY KEY ( f_id ),
          KEY  f_id  ( f_id )
-        ) ENGINE=InnoDB DEFAULT CHARSET=utf8
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
